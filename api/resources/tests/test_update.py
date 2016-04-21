97719dfa930fdd1fe92f6b67b366a519c786a9ea
tomaszroszko <tomaszroszko@tjsoftware.co>
2016-04-21 23:05:54 +0200

    Add basic update view (no refs)

---
+++ api/resources/tests/test_update.py
@@ -0,0 +1,116 @@
+import unittest
+
+from falcon import Request, Response
+from falcon.testing import create_environ
+
+from api.resources.update import UpdateResource
+
+
+class FakeObject(object):
+    """
+    Fake object to mock instance of document
+    """
+
+    def __init__(self, pk):
+        self.pk = pk
+        self.name = 'OldName'
+        self._saved = False
+
+    def save(self):
+        self._saved = True
+        return self
+
+
+class FakeObjectList(object):
+    """
+    Fake object list used to mock manager behaviour
+    """
+
+    def get(self, pk):
+        return FakeObject(pk=pk)
+
+
+def fake_to_json(obj):
+    """fake object to json func"""
+    return {
+        'pk': obj.pk,
+        'name': obj.name,
+        '_saved': obj._saved
+    }
+
+
+class UpdateResourceTest(unittest.TestCase):
+    """
+    Testcase for :class:`api.resources.create.CreateResource`
+    """
+
+    def test_update_withtout_pk(self):
+        """
+        test how update function will handle when we are not going to pass pk value
+        """
+        resource = UpdateResource(objects_class=None, expected_params=[])
+        env = create_environ(path='/')
+        req = Request(env)
+        req.context = {
+            'doc': {}
+        }
+        resp = Response()
+
+        resource.on_patch(req, resp)
+        self.assertEqual(req.context['result']['errors'], ['PK param was not provided'])
+
+    def test_update_get_object(self):
+        """
+        Test `get_object` func
+        """
+        resource = UpdateResource(objects_class=None, expected_params=[])
+        env = create_environ(path='/')
+        req = Request(env)
+        req.context = {
+            'doc': {'pk': 1}
+        }
+        resp = Response()
+
+        resource.objects_class = FakeObjectList()
+        obj = resource.get_object(req=req, resp=resp)
+        self.assertEqual(obj.pk, 1)
+
+    def test_update_when_no_expected_params_is_set(self):
+        """
+        Test if update func will not update param if it's not defined in expected params
+        """
+        resource = UpdateResource(objects_class=None, expected_params=[])
+        env = create_environ(path='/')
+        req = Request(env)
+        req.context = {
+            'doc': {'pk': 1, 'name': 'TestNewName'}
+        }
+        resp = Response()
+
+        resource.objects_class = FakeObjectList()
+        resource.to_json = fake_to_json
+        resource.on_patch(req, resp)
+        self.assertEqual(
+            {'pk': 1, 'name': 'OldName', '_saved': True},
+            req.context['result']
+        )
+
+    def test_update_params(self):
+        """
+        Test if param is updated and returned
+        """
+        resource = UpdateResource(objects_class=None, expected_params=['name'])
+        env = create_environ(path='/')
+        req = Request(env)
+        req.context = {
+            'doc': {'pk': 1, 'name': 'TestNewName'},
+        }
+        resp = Response()
+
+        resource.objects_class = FakeObjectList()
+        resource.to_json = fake_to_json
+        resource.on_patch(req, resp)
+        self.assertEqual(
+            {'pk': 1, 'name': 'TestNewName', '_saved': True},
+            req.context['result']
+        )
