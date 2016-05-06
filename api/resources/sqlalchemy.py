d00203cf7b64a3292cefa24ba67fbd766e9c6308
Jan Waś <janwas@tjsoftware.co>
2016-05-06 15:40:33 +0200

    allow to skip keys in sql alchemy serializer (no refs)

--- api/resources/sqlalchemy.py
+++ api/resources/sqlalchemy.py
@@ -38,7 +38,7 @@ class AlchemyMixin(object):
         data = {}
         columns = inspect(obj).mapper.columns
         for key, column in columns.items():
-            if not skip_keys and (column.primary_key or isinstance(column.type, schema.ForeignKey)):
+            if skip_keys and (column.primary_key or isinstance(column.type, schema.ForeignKey)):
                 continue
             value = getattr(obj, key)
             if isinstance(value, datetime):
