01a6638d7e161c69ef852fda4cc1807ca67c19ab
Jan Waś <janwas@tjsoftware.co>
2016-06-23 18:06:14 +0200

    Ignore relations param when fetching a single record (no refs)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='opentopic-falcon-api',
-    version='0.1.2',
+    version='0.1.3',
     author='Tomasz Roszko',
     author_email='tom@opentopic.com',
     description='Base Library for services api endpoints',
