7f7e29e61010c18f70a639c484e86dcd15ee770d
Jan Waś <janwas@tjsoftware.co>
2016-07-04 14:31:36 +0200

    Bump version before release (no refs)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='opentopic-falcon-api',
-    version='0.2.1',
+    version='0.2.2',
     author='Tomasz Roszko',
     author_email='tom@opentopic.com',
     description='Base Library for services api endpoints',
