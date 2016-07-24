d048e1f5534c0eb1da7b744e9bc3f64dc27c781c
John Was <janek.jan@gmail.com>
2016-07-24 11:44:12 +0200

    Don't try to create new one to one relations even if pk is missing (no refs)

--- api/resources/sqlalchemy.py
+++ api/resources/sqlalchemy.py
@@ -583,7 +583,8 @@ class AlchemyMixin(object):
                 rel_obj = getattr(obj, key)
                 if isinstance(value, dict):
                     relationship = mapper.relationships[key]
-                    if relationship.direction != MANYTOONE and (pk not in value or rel_obj is None):
+                    if (relationship.direction == MANYTOONE or relationship.uselist)\
+                            and (pk not in value or rel_obj is None):
                         setattr(obj, key, AlchemyMixin.update_or_create(db_session, related_mapper, value))
                     else:
                         AlchemyMixin.save_resource(rel_obj, value, db_session)
