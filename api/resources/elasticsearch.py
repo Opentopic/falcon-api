fa4517545500e70a61e78c72d8a2e4bef8e1c762
Jan Waś <janwas@tjsoftware.co>
2016-09-28 12:55:11 +0200

    allow to override default search object in ES resource (refs #32008)

--- api/resources/elasticsearch.py
+++ api/resources/elasticsearch.py
@@ -250,10 +250,13 @@ class CollectionResource(ElasticSearchMixin, BaseCollectionResource):
         super(CollectionResource, self).__init__(objects_class, max_limit)
         self.connection = connection
 
+    def get_base_query(self):
+        return Search(using=self.connection,
+                      index=self.objects_class._doc_type.index,
+                      doc_type=self.objects_class)
+
     def get_queryset(self, req, resp):
-        query = Search(using=self.connection,
-                       index=self.objects_class._doc_type.index,
-                       doc_type=self.objects_class)
+        query = self.get_base_query()
 
         if self.PARAM_SEARCH in req.params:
             try:
