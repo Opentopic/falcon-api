3efd4ca0aa09208408412a464d75674eb7a79160
Jan Waś <janwas@tjsoftware.co>
2017-03-27 11:04:39 +0200

    split and move error handlers to middlewares

--- api/middlewares/json_middleware.py
+++ api/middlewares/json_middleware.py
@@ -1,5 +1,6 @@
 import rapidjson as json
 import falcon
+import logging
 
 
 class RequireJSON(object):
@@ -75,3 +76,39 @@ class JSONTranslator(object):
         :type resource: api.resources.base.BaseResource
         """
         resp.body = json.dumps(resp.body)
+
+
+class JsonError(Exception):
+    """
+    Converts all exceptions to falcon.HTTPError so they're serializable and match requested content type.
+
+    Usage:
+
+    .. code-block:: python
+
+        app.add_error_handler(Exception, JsonError.handle)
+    """
+    @staticmethod
+    def handle(ex, req, resp, params):
+        """
+        :param ex: the exception
+        :type ex: Exception
+
+        :param req: Falcon request
+        :type req: falcon.request.Request
+
+        :param resp: Falcon response
+        :type resp: falcon.response.Response
+
+        :param params: parameters dict
+        :type params: dict
+
+        :raises falcon.HTTPError
+        """
+        logger = logging.getLogger()
+        if not isinstance(ex, falcon.HTTPError):
+            logger.error('Non-HTTP exception occurred ({}): {}'.format(type(ex), ex), exc_info=ex)
+            ex = falcon.HTTPError(falcon.HTTP_500, str(type(ex)), str(ex))
+        else:
+            logger.warning('HTTP exception occurred ({}): {}'.format(type(ex), ex), exc_info=ex)
+        raise ex
