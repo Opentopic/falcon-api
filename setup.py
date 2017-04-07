69c0c956b4945c80100bb65dc3fe652ef719112c
Jan Waś <janwas@tjsoftware.co>
2017-04-07 14:53:26 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='opentopic-falcon-api',
-    version='1.1.4',
+    version='1.1.5',
     author='Jan Waś',
     author_email='jan.was@opentopic.com',
     description='Falcon API resources for databases',
