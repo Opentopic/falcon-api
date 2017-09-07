13f08ad5ba720cb1c3d048ecc677d5c810f1356b
Jan Waś <janwas@tjsoftware.co>
2017-09-07 14:24:58 +0200

    properly create a new 1:1 object when creating a resource

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='opentopic-falcon-api',
-    version='1.1.21',
+    version='1.1.22',
     author='Jan Waś',
     author_email='jan.was@opentopic.com',
     description='Falcon API resources for databases',
